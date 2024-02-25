8325def529f929bef5157e9c1907a3b1363702fb
Ierga <i.ergakoff@gmail.com>
2024-02-25 20:23:29 +0300

    GB - Spring. Homework 8. Start

---
+++ src/main/java/ru/ergakov/gb/GbSpringHomework8Application.java
@@ -0,0 +1,13 @@
+package ru.ergakov.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringHomework8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbSpringHomework8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ergakov/gb/GbSpringHomework8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ergakov.gb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringHomework8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
